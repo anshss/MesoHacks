7cc624521cfc56312c824636b4f06b9194ade681
Ansh <anshsaxena4190@gmail.com>
2022-09-03 02:47:15 +0530

    contract added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle"); 
+const fs = require('fs'); 
+const privateKey = fs.readFileSync(".secret").toString() 
+const infuraId = fs.readFileSync(".infuraId").toString()
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.9",
-};
+module.exports = { 
+  defaultNetwork : "hardhat", 
+  networks: { 
+    hardhat: { 
+      chainId: 1337 
+    }, 
+    rinkeby: { 
+      url: `https://rinkeby.infura.io/v3/${infuraId}`, 
+      accounts: [privateKey] 
+    }, 
+  }, 
+  solidity: { 
+    version: "0.8.4", 
+  } 
+}
