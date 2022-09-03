cb2949145fcff467d8e71bd3292b360d0b08e7c5
Sarthak Vaish <sarthakvaish184@gmail.com>
2022-09-03 20:20:54 +0530

    added hr in navigation bar

--- components/Nav.js
+++ components/Nav.js
@@ -6,40 +6,43 @@ export default function Nav() {
   const [active, setActive] = React.useState(true);
 
   return (
-    <div className={styles.container}>
-      {active ? (
-        <>
-          <Link href="/">
-            <img
-              className={styles.icon}
-              src="https://img.icons8.com/material-rounded/48/000000/dashboard-layout.png"
-            />
-          </Link>
-          <Link href="/createpost">
-            <img
-              onClick={() => setActive(false)}
-              className={styles.icon2}
-              src="https://img.icons8.com/48/FFFFFF/create-new.png"
-            />
-          </Link>
-        </>
-      ) : (
-        <>
-          <Link href="/">
-            <img
-              onClick={() => setActive(true)}
-              className={styles.icon2}
-              src="https://img.icons8.com/material-rounded/48/FFFFFF/dashboard-layout.png"
-            />
-          </Link>
-          <Link href="/createpost">
-            <img
-              className={styles.icon}
-              src="https://img.icons8.com/48/000000/create-new.png"
-            />
-          </Link>
-        </>
-      )}
+    <div className={styles.navbar}>
+      <div className={styles.container}>
+        {active ? (
+          <>
+            <Link href="/">
+              <img
+                className={styles.icon}
+                src="https://img.icons8.com/material-rounded/48/000000/dashboard-layout.png"
+              />
+            </Link>
+            <Link href="/createpost">
+              <img
+                onClick={() => setActive(false)}
+                className={styles.icon2}
+                src="https://img.icons8.com/48/FFFFFF/create-new.png"
+              />
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <img
+                onClick={() => setActive(true)}
+                className={styles.icon2}
+                src="https://img.icons8.com/material-rounded/48/FFFFFF/dashboard-layout.png"
+              />
+            </Link>
+            <Link href="/createpost">
+              <img
+                className={styles.icon}
+                src="https://img.icons8.com/48/000000/create-new.png"
+              />
+            </Link>
+          </>
+        )}
+      </div>
+      <hr />
     </div>
   );
 }
