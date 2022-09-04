350c456afcb49f001df6b649dbcd7d8cbede200e
Sarthak Vaish <sarthakvaish184@gmail.com>
2022-09-04 14:46:06 +0530

    made slider dynamic

--- components/Slider.js
+++ components/Slider.js
@@ -4,8 +4,8 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import { EffectCoverflow, Pagination } from "swiper";
 import styles from "../styles/Slider.module.css";
-
- const Slider = () => {
+import SingleSlide from "./singleSlide";
+const Slider = ({ newsData }) => {
   return (
     <div className={styles.sliderPage}>
       <style jsx global>
@@ -35,136 +35,11 @@ import styles from "../styles/Slider.module.css";
         modules={[EffectCoverflow, Pagination]}
         className={styles.mySwiper}
       >
-        <SwiperSlide>
-          <div className={styles.newsCard}>
-            <div className={styles.topBar}>
-              <img src="https://i.pravatar.cc/150?u=fake@pravatar.com" />
-              <div className={styles.heading}>
-                <p>#02lsdf02rl</p>
-                <h2>Title for the News. It will look something like this!</h2>
-              </div>
-            </div>
-            <div className={styles.content}>
-              <p>
-                Description for the News. It will look something like this! and
-                this will go on and on and on and on and on and.... on and on
-                and on. Yeah, This will go on and on and on and on and on
-                and.... on and on and on.....{" "}
-              </p>
-            </div>
-            <div className={styles.footer}>
-              <a href="#">Read More</a>
-              <div>
-                <img src="" />
-                <img src="" />
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={styles.newsCard}>
-            <div className={styles.topBar}>
-              <img src="https://i.pravatar.cc/150?u=fake@pravatar.com" />
-              <div className={styles.heading}>
-                <p>#02lsdf02rl</p>
-                <h2>Title for the News. It will look something like this!</h2>
-              </div>
-            </div>
-            <div className={styles.content}>
-              <p>
-                Description for the News. It will look something like this! and
-                this will go on and on and on and on and on and.... on and on
-                and on. Yeah, This will go on and on and on and on and on
-                and.... on and on and on.....{" "}
-              </p>
-            </div>
-            <div className={styles.footer}>
-              <a href="#">Read More</a>
-              <div>
-                <img src="" />
-                <img src="" />
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={styles.newsCard}>
-            <div className={styles.topBar}>
-              <img src="https://i.pravatar.cc/150?u=fake@pravatar.com" />
-              <div className={styles.heading}>
-                <p>#02lsdf02rl</p>
-                <h2>Title for the News. It will look something like this!</h2>
-              </div>
-            </div>
-            <div className={styles.content}>
-              <p>
-                Description for the News. It will look something like this! and
-                this will go on and on and on and on and on and.... on and on
-                and on. Yeah, This will go on and on and on and on and on
-                and.... on and on and on.....{" "}
-              </p>
-            </div>
-            <div className={styles.footer}>
-              <a href="#">Read More</a>
-              <div>
-                <img src="" />
-                <img src="" />
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={styles.newsCard}>
-            <div className={styles.topBar}>
-              <img src="https://i.pravatar.cc/150?u=fake@pravatar.com" />
-              <div className={styles.heading}>
-                <p>#02lsdf02rl</p>
-                <h2>Title for the News. It will look something like this!</h2>
-              </div>
-            </div>
-            <div className={styles.content}>
-              <p>
-                Description for the News. It will look something like this! and
-                this will go on and on and on and on and on and.... on and on
-                and on. Yeah, This will go on and on and on and on and on
-                and.... on and on and on.....{" "}
-              </p>
-            </div>
-            <div className={styles.footer}>
-              <a href="#">Read More</a>
-              <div>
-                <img src="" />
-                <img src="" />
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={styles.newsCard}>
-            <div className={styles.topBar}>
-              <img src="https://i.pravatar.cc/150?u=fake@pravatar.com" />
-              <div className={styles.heading}>
-                <p>#02lsdf02rl</p>
-                <h2>Title for the News. It will look something like this!</h2>
-              </div>
-            </div>
-            <div className={styles.content}>
-              <p>
-                Description for the News. It will look something like this! and
-                this will go on and on and on and on and on and.... on and on
-                and on. Yeah, This will go on and on and on and on and on
-                and.... on and on and on.....{" "}
-              </p>
-            </div>
-            <div className={styles.footer}>
-              <a href="#">Read More</a>
-              <div>
-                <img src="" />
-                <img src="" />
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
+        {newsData.map((item, index) => (
+          <SwiperSlide>
+            <SingleSlide key={index} index={index} data={item} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
---
+++ components/singleSlide.js
@@ -0,0 +1,27 @@
+import styles from "../styles/Slider.module.css";
+
+const SingleSlide = ({ data, index }) => {
+  return (
+    <div className={styles.newsCard}>
+      <div className={styles.topBar}>
+        <img
+          src={
+            "https://avatars.dicebear.com/api/micah/" + data.address + ".svg"
+          }
+        />
+        <div className={styles.heading}>
+          <p>#{data.address}</p>
+          <h2>{data.title}</h2>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p>{data.content}</p>
+      </div>
+      <div className={styles.footer}>
+        <img src="./like.png" />
+        <img src="./support.png" />
+      </div>
+    </div>
+  );
+};
+export default SingleSlide;
